641c80a8786e3022d2c99803fb24218835e48a0d
Hafsa-Begum <hafsagood@gmail.com>
2022-02-01 22:09:27 +0600

    header added

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { logoutInitiate } from '../../redux/actions';
 
 const Header = () => {
@@ -19,10 +20,27 @@ const Header = () => {
 
     return (
         <div>
-            <h2>This is header</h2>
-            {
-                currentUser?.displayName && <button onClick={handleLogout} className='btn btn-danger'>Logout</button>
-            }
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/home"><img src="https://i.ibb.co/02mVbb3/logo-1.png" alt="" /> <span className='fst-italic'>Smart <sub>Contact Manager</sub> </span></Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <NavLink className='nav-link' to="/home"><i className="fs-5 fas fa-home"></i> Home</NavLink>
+                        {
+                            currentUser?.displayName ? <>
+                                <NavLink className='nav-link' to="/contactList"><i className="fs-5 fas fa-address-book"></i> Contact List</NavLink>
+                                <button onClick={handleLogout} className="btn btn-danger">Logout <i className="fs-5 fas fa-sign-out-alt"></i></button>
+                            </>
+                                :
+                                <NavLink className='nav-link' to="/signin"><i className="fs-5 fas fa-sign-in-alt"></i> Signin</NavLink>
+                        }
+                        {currentUser?.displayName &&
+                            <Navbar.Text>
+                                <h5 className='ms-4 text-white'>Hi, {currentUser?.displayName}</h5>
+                            </Navbar.Text>}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { logoutInitiate } from '../redux/actions';
 
 const Home = () => {
-    // const navigate = useNavigate();
-    // const { currentUser } = useSelector((state) => state.user);
-    // const dispatch = useDispatch();
-
-    // const handleLogout = () => {
-    //     if (currentUser) {
-    //         dispatch(logoutInitiate());
-    //         navigate("/")
-    //     }
-    // }
 
     return (
-        <div>
-            <h2 className='text-success'>this is a contact management app</h2>
-            {/* <button onClick={handleLogout} className='btn btn-danger'>Logout</button> */}
+        <div style={{ height: '100vh', textAlign: 'center' }}>
+            <h2 className='text-success'>Welcome to contact management app</h2>
         </div>
     );
 };
